2e89e3b2572c09798d32b9d1753dc3714600b5f9
Hocine Bourouih <hocine.bourouih@outlook.fr>
2024-01-06 15:14:15 +0100

    histograme and data updated

--- main.py
+++ main.py
@@ -36,14 +36,17 @@ df = lire_donnees_json()
 
 app.layout = html.Div([
     html.H1("Dashboard des Offres d'Emploi par Département"),
-    dcc.Slider(
-        id='min-offre-slider',
-        min=0,
-        max=df['Departement'].value_counts().max(),
-        value=5,
-        marks={i: str(i) for i in range(0, df['Departement'].value_counts().max() + 1, 5)},
-        step=1
-    ),
+    html.Div([
+        html.Label("Afficher uniquement les départements ayant un nombre d'offres supérieur ou égal à la valeur choisie : "),
+        dcc.Slider(
+            id='min-offre-slider',
+            min=0,
+            max=df['Departement'].value_counts().max(),
+            value=5,
+            marks={i: str(i) for i in range(0, df['Departement'].value_counts().max() + 1, 5)},
+            step=1
+        ),
+    ], style={'padding': '20px', 'margin': '20px'}),
     html.Div(id='graph-container')
 ])
 
