926358190af357f77ca5662919374e852e112073
Hocine Bourouih <hocine.bourouih@outlook.fr>
2024-01-06 18:42:23 +0100

    script qui permet de récupérer les fichier data pour éviter de les stocker sur git

--- get_data.py
+++ get_data.py
@@ -12,11 +12,17 @@ def save_data_to_file(data, filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
-url = "https://www.data.gouv.fr/fr/datasets/r/0aba1710-168c-4ec2-a6e2-4ac95df8a63d"
-data = get_data(url)
+urls = [
+    "https://drive.google.com/uc?id=1V4rR4F1seaqc0T7aHup1tWPoLQJ6acN3",
+    "https://drive.google.com/uc?id=18qQVWj-UnGu6CeNYXmA4oBVUJZZxbS1j",
+    "https://drive.google.com/uc?id=18qQVWj-UnGu6CeNYXmA4oBVUJZZxbS1j"
+]
 
-if data:
-    save_data_to_file(data, 'niveau.json')
-    print("Données téléchargées et sauvegardées dans 'niveau.json'")
-else:
-    print("Erreur lors du téléchargement des données.")
+for i, url in enumerate(urls):
+    data = get_data(url)
+    if data:
+        filename = f'data_{i}.json'
+        save_data_to_file(data, filename)
+        print(f"Données téléchargées et sauvegardées dans '{filename}'")
+    else:
+        print(f"Erreur lors du téléchargement des données depuis {url}.")
